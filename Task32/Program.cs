909fceb28f2ac2b4f9e7a8fd92dbe73ffbf20d24
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-27 14:07:01 +0500

    Task31

---
+++ Task32/Program.cs
@@ -0,0 +1,26 @@
+﻿// //Task32 Задайте массив из элементов, заполненный случайными числами из промежутка [-9, 9].
+// Положительные элементы заменить на отрицательные и наборот
+
+
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(-9, 10); // [-9, 9]
+}
+
+
+void ReleaseArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   array[i] *= (-1);
+  
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
